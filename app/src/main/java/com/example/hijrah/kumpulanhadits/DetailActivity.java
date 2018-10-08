a1a0fad94ea293c24335d3f9de2d936106aa5467
jarvis000611 <ironman000611@gmail.com>
2018-10-08 03:11:32 -0700

    second commit

---
+++ app/src/main/java/com/example/hijrah/kumpulanhadits/DetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.hijrah.kumpulanhadits;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hijrah on 10/1/2018.
+ */
+
+public class DetailActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
---
+++ app/src/main/java/com/example/hijrah/kumpulanhadits/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.hijrah.kumpulanhadits;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hijrah on 10/1/2018.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
